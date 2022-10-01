1db539762a8e86e449feb2345cd657fbc905d736
Aaron Ibarra <aaronibarra132@gmail.com>
2022-10-01 16:36:37 -0700

    Added color button and other little style edits

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 // grab DOM elements
 
 // set event listeners
+// getElementById
+const nameDisplay = document.getElementById('name');
+console.log(nameDisplay);
+
+//DOM SETTER METHODS
+//textContent
+nameDisplay.textContext = 'Aaron';
+nameDisplay.classList.add('black');
+
+const updateBtn = document.getElementById('update-btn');
+updateBtn.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    //grab the input element
+    const nameInput = document.getElementById('name-input');
+    nameDisplay.textContent = nameInput.value;
+});
+
+const updateBtn2 = document.getElementById('update-btn2');
+updateBtn2.addEventListener('click', () => {
+    const colorInput = document.getElementById('color-input');
+});
